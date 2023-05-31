d899834dfbf3ecab834328053d795fd717b6d44c
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-05-31 18:07:05 +0530

    Create the addProjectModalElement function and export it

--- src/page-load.js
+++ src/page-load.js
@@ -1,3 +1,5 @@
+import { newModal } from "./modal";
+
 const headerElement = () => {
     const header = document.createElement("header");
     header.classList.add("header");
@@ -43,9 +45,24 @@ const footerElement = () => {
 
     return footer;
 }
+const addProjectModalElement = () => {
+    const addProjectModal = newModal();
+    addProjectModal.classList.add("add-project-modal");
+    const modal = document.querySelector(".add-project-modal > .modal");
+    modal.classList.add("add-project");
+    const overlay = document.querySelector(".add-project-modal > .overlay");
+    overlay.classList.add("add-project");
+    const projectNameInput = document.createElement("input");
+    projectNameInput.setAttribute("type", "text");
+    projectNameInput.setAttribute("id", "project-name");
+    projectNameInput.setAttribute("placeholder", "Project Name");
+
+    return addProjectModal;
+}
 
 export {
     headerElement,
     mainElement,
-    footerElement
+    footerElement,
+    addProjectModalElement
 }
