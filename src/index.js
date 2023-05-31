9195aec05e38ace8eafeb26c979ebc78278cab2b
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-05-31 18:04:13 +0530

    Append addProjectModal in the body

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import { headerElement, mainElement, footerElement } from "./page-load";
+import { headerElement, mainElement, footerElement, addProjectModalElement } from "./page-load";
 import todo from "./todo";
 import project from "./project";
-import { onInboxClick } from "./on-click";
+import { onInboxClick, onAddProjectClick } from "./on-click";
 import "./style.css"
 
 const header = headerElement();
 const main = mainElement();
 const footer = footerElement();
+const addProjectModal = addProjectModalElement();
 
 document.body.appendChild(header);
 document.body.appendChild(main);
+document.body.appendChild(addProjectModal);
 document.body.appendChild(footer);
 
 const inbox = project("inbox");
 
+const projects = [];
+
 const inboxBtn = document.querySelector(".inbox");
+const addProjectBtn = document.querySelector(".add-project-btn")
 const todoDisplay = document.querySelector(".todoDisplay");
 
 inboxBtn.addEventListener("click", () => onInboxClick(todoDisplay));
+addProjectBtn.addEventListener("click", () => onAddProjectClick());
